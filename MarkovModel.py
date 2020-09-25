047a6ce9a0b5d4dde5cf835daa1c125ba7eb9298
ggoh29 <56758342+ggoh29@users.noreply.github.com>
2020-09-25 21:20:42 +0800

    Update MarkovModel.py

--- MarkovModel.py
+++ MarkovModel.py
@@ -1,6 +1,7 @@
 import math
 import operator
 
+
 # rules for input:
 # The hidden layer must have a length greater than the emission layer by a size of 2. This is to account for the start
 # end symbol. The start and end symbol cannot exist anywhere inside the hidden layer except at the start and the end.
@@ -12,7 +13,6 @@ import operator
 #
 
 class MarkovModel:
-
     all_emitted_characters = {}
     all_hidden_characters = {}
 
@@ -37,15 +37,15 @@ class MarkovModel:
                 raise Exception("Hidden layer must have a length greater than the emission layer by a size of 2")
             for i in range(len(emitted)):
                 self.all_emitted_characters[emitted[i]] = 0
-                if hidden[i+1] == 's' or hidden[i+1] == 'e':
+                if hidden[i + 1] == 's' or hidden[i + 1] == 'e':
                     raise Exception("Hidden layer cannot have the symbol 's' or 'e' except at the start and end")
                 else:
-                    self.all_hidden_characters[hidden[i+1]] = 0
+                    self.all_hidden_characters[hidden[i + 1]] = 0
 
             if hidden[0] != 's':
                 raise Exception("Start of hidden layer must be 's'")
 
-            if hidden[len(hidden)-1] != 'e':
+            if hidden[len(hidden) - 1] != 'e':
                 raise Exception("End of hidden layer must be 'e'")
 
         self.all_hidden_characters['s'] = 0
@@ -67,7 +67,7 @@ class MarkovModel:
                 for character in self.emission_matrix[symbol]:
                     if character != 's' or character != 'e':
                         self.emission_matrix[symbol][character] = 1
-                        self.symbol_count_with_smoothing [symbol] += 1
+                        self.symbol_count_with_smoothing[symbol] += 1
         for index, row in df.iterrows():
             self.build_matrix(row[0], row[1])
         self.get_probabilities()
@@ -80,10 +80,10 @@ class MarkovModel:
         '''
         transaction_txt_len = len(transaction_txt)
         current_symbol = transaction_hidden[0]
-        for i in range(1, transaction_txt_len+1):
+        for i in range(1, transaction_txt_len + 1):
             next_symbol = transaction_hidden[i]
             self.transition_matrix[current_symbol][next_symbol] += 1
-            self.emission_matrix[next_symbol][transaction_txt[i-1]] += 1
+            self.emission_matrix[next_symbol][transaction_txt[i - 1]] += 1
             self.symbol_count_with_smoothing[next_symbol] += 1
             self.symbol_count_no_smoothing[next_symbol] += 1
             current_symbol = next_symbol
@@ -112,7 +112,7 @@ class MarkovModel:
                 self.transition_matrix['s'][start_transition] = math.log(self.transition_matrix['s'][start_transition])
 
     @staticmethod
-    def predict_by_viterbi(transaction_txt, emission_matrix, transition_matrix, to_consider=1, constraints = None):
+    def predict_by_viterbi(transaction_txt, emission_matrix, transition_matrix, to_consider=1, constraints=None):
         '''
         :param transaction_txt: text to be examined
         :return: Returns the most likely sequence of states
@@ -133,7 +133,8 @@ class MarkovModel:
             for state_cur in possible_states:
                 mx = -math.inf
                 for state_prev in possible_states:
-                    cur_mx = previous[state_prev] + emission_matrix[state_cur][cur_emission] + transition_matrix[state_prev][state_cur]
+                    cur_mx = previous[state_prev] + emission_matrix[state_cur][cur_emission] + \
+                             transition_matrix[state_prev][state_cur]
                     if cur_mx > mx:
                         best_prev = state_prev
                         mx = cur_mx
@@ -214,7 +215,7 @@ class MarkovModel:
                 MarkovModel.quick_sort(lst, start, counter)
 
     @staticmethod
-    def predict_by_rules(transaction_txt,emission_matrix, transition_matrix, to_consider, constraints):
+    def predict_by_rules(transaction_txt, emission_matrix, transition_matrix, to_consider, constraints):
         '''
         :param transaction_txt: text to be examined
         :param to_consider: number of sequences we will examine to see if they satisfy the constraint
@@ -229,7 +230,7 @@ class MarkovModel:
             start = {symbol: transition_matrix['s'][symbol] + emission_matrix[symbol][transaction_txt[0]]}
             queue.append(start)
 
-        end_transition_dict = {txt_len-1: [transition_matrix[symbol]['e'] for symbol in possible_states]}
+        end_transition_dict = {txt_len - 1: [transition_matrix[symbol]['e'] for symbol in possible_states]}
         # Using -1 to demarcate a new level in the tree
         queue.append(-1)
         i = 1
@@ -245,15 +246,15 @@ class MarkovModel:
             else:
                 cur_node = queue[0]
                 prev_chain = list(cur_node.keys())[0]
-                prev_symbol = prev_chain[i-1]
+                prev_symbol = prev_chain[i - 1]
                 prev_prob = cur_node[prev_chain]
                 cur_emission = transaction_txt[i]
                 j = 0
                 for symbol in possible_states:
                     temp = {prev_chain + symbol: prev_prob
-                                                  + transition_matrix[prev_symbol][symbol]
-                                                  + emission_matrix[symbol][cur_emission]
-                                                  + end_transition_dict.get(i, [0 for i in possible_states])[j]}
+                                                 + transition_matrix[prev_symbol][symbol]
+                                                 + emission_matrix[symbol][cur_emission]
+                                                 + end_transition_dict.get(i, [0 for i in possible_states])[j]}
                     queue.append(temp)
                     j += 1
                 queue.pop(0)
@@ -261,24 +262,27 @@ class MarkovModel:
         size_of_leaves = len(queue)
         queue.pop(size_of_leaves - 1)
         if 0 < to_consider < len(queue):
-            MarkovModel.quick_partition(queue, 0, size_of_leaves-1, to_consider)
+            MarkovModel.quick_partition(queue, 0, size_of_leaves - 1, to_consider)
             MarkovModel.quick_sort(queue, 0, to_consider)
             queue = queue[:to_consider]
         else:
-            MarkovModel.quick_sort(queue, 0, size_of_leaves-1)
+            MarkovModel.quick_sort(queue, 0, size_of_leaves - 1)
         for possible_chain in queue:
             current_chain = list(possible_chain.keys())[0]
             satisfies_constrains = True
             for fun in constraints:
-                satisfies_constrains = satisfies_constrains and bool(fun.__func__(current_chain))
+                satisfies_constrains = satisfies_constrains and bool(fun(current_chain))
             if satisfies_constrains:
                 return current_chain
 
         # If none satisfies the constraints, return the most likely sequence which is the first in the queue
         return list(queue[0].keys())[0]
 
+    @staticmethod
+    def wrong_method(transaction_txt, emission_matrix, transition_matrix, to_consider, constraints):
+        raise Exception("No such input method")
 
-    def predict(self, transaction_txt, method = predict_by_viterbi, to_consider = 1000, constraints = []):
+    def predict(self, transaction_txt, method="predict_by_viterbi", to_consider=1000, constraints=[]):
         '''
         :param transaction_txt: text to be examined
         :param method: method by which to extract best sequence
@@ -291,7 +295,11 @@ class MarkovModel:
         predict_by_rules returns the best possible sequence that satisfies some constraints
         :return: a sequence of symbols
         '''
-        sequence = method.__func__(transaction_txt, self.emission_matrix, self.transition_matrix, to_consider, constraints)
+        methods = {"predict_by_viterbi":MarkovModel.predict_by_viterbi,
+                   "predict_by_rules": MarkovModel.predict_by_rules}
+        method = methods.get(method, MarkovModel.wrong_method)
+        sequence = method(transaction_txt, self.emission_matrix, self.transition_matrix, to_consider,
+                                   constraints)
 
         return sequence
 
